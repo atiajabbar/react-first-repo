d49427ba80c9a46f3f4a5b4ffc0d2e78c0849ce7
AJkhan <atiajabbar39@gmail.com>
2023-03-20 23:20:07 +0500

    Add files via upload

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Message from './Message.js'; 
+
+const App = () => {
+  return (
+    <>
+    <Message /> 
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Message.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+import "./App.css";
+
+
+const Message = () => {
+    
+    return(
+    <div className="mainDiv">
+       <h1>Hello! This is the First React App based on Function Method</h1>
+    </div>
+    );
+}
+
+ 
+export default Message;
